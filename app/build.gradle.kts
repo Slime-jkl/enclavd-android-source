fa5cfab69fb8b174de3d4d35c002d88989881e84
Slime-jkl <ionutbr95@gmail.com>
2026-06-07 22:58:06 +0100

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.enclavd.app"
-	compileSdk = 36
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.enclavd.app"
@@ -16,15 +16,27 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        release {
+            // Reads the environment variables generated during your GitHub Actions run
+            storeFile = System.getenv("SIGNING_KEY_FILE") ? file(System.getenv("SIGNING_KEY_FILE")) : null
+            storePassword = System.getenv("KEY_STORE_PASSWORD")
+            keyAlias = System.getenv("ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.release // Injects the signing keys into the release build
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
