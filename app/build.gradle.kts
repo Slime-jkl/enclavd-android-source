cef8bda4a5855881c2e855c3c60c11d18468ef49
Slime-jkl <ionutbr95@gmail.com>
2026-06-23 10:57:24 +0100

    Play store adjustment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.enclavd.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
+        versionCode = 008
         versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
